db81286675f4cefe340236c1c5affd1a04596fcd
John Doe <johndoe@example.com>
2022-11-15 14:58:34 +0100

    Elso feladat kész

--- src/index.js
+++ src/index.js
@@ -1,2 +1,37 @@
 import './style.css';
 
+
+async function osszesMegjelenit(idezetlista){
+    let lista = document.getElementById('idezetek');
+    for (let e of idezetlista){
+        let li = document.createElement('li');
+        li.textContent = e.author + ", " + e.quote;
+        lista.appendChild(li);
+    }
+}
+
+function darabszamMegjelenit(){
+    document.getElementById('szerzo')
+    
+}
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('osszes').addEventListener('click', async() => {
+        let response = await fetch('./quotes.json');
+        let eredmeny = await response.json();
+        let osszes = eredmeny.quotes;
+        osszes = osszes.sort((a, b) => {
+            if (a.author < b.author) {
+              return -1;
+            }
+        });
+        osszesMegjelenit(osszes);       
+        
+    });
+
+    
+
+});
+
